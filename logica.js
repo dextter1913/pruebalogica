11d635dbf2254f72ae5a5dd47a2e92b216e2259f
dextter1913 <dextter1913@gmail.com>
2021-06-21 00:46:46 -0500

    ultimo comit

--- logica.js
+++ logica.js
@@ -2,15 +2,15 @@
 Escribe un programa en JavaScript que imprima el número más alto del arreglo (Si se repite,
 solo imprimir una vez).*/
 
-var MyArray = [40,2,4,35,1]
-var M = 0
-for(i = 0; i < MyArray.length; i++){
-    if (MyArray[i] > M)
-    {
-        M = MyArray[i]
-    }
-}
-console.log(M);
+//ar MyArray = [40,2,4,35,1]
+//ar M = 0
+//or(i = 0; i < MyArray.length; i++){
+//   if (MyArray[i] > M)
+//   {
+//       M = MyArray[i]
+//   }
+//
+//onsole.log(M);
 
 
 
@@ -22,28 +22,28 @@ El arreglo se llama myArray y contiene 10 elementos que corresponden a números 
 del 1 al 5. Un histograma representa que tanto un elemento aparece en un conjunto de
 datos. Por ejemplo, para el arreglo: myArray:=(1,2,1,3,3,1,2,1,5,1) el histograma se vería así:*/
 
-let arr = [7, 7, 7, 7, 2, 4, 6, 8, 2]
-
-    function imprimir(array) {
-
-
-    let arrToCompare = []
-    array.forEach((x, i) => {
-        arrToCompare.includes(x) ? "" : arrToCompare.push(x)
-    })
-
-    arrToCompare.forEach(function (x) {
-        let arrToJoin = []
-        array.forEach(function (i) {
-        if (i === x) {
-            arrToJoin.push('*')
-        }
-        })
-        console.log(x, arrToJoin.join(''))
-    })
-    }
-
-    imprimir(arr)
+//let arr = [7, 7, 7, 7, 2, 4, 6, 8, 2]
+//
+//    function imprimir(array) {
+//
+//
+//    let arrToCompare = []
+//    array.forEach((x, i) => {
+//        arrToCompare.includes(x) ? "" : arrToCompare.push(x)
+//    })
+//
+//    arrToCompare.forEach(function (x) {
+//        let arrToJoin = []
+//        array.forEach(function (i) {
+//        if (i === x) {
+//            arrToJoin.push('*')
+//        }
+//        })
+//        console.log(x, arrToJoin.join(''))
+//    })
+//    }
+//
+//    imprimir(arr)
 
 
 
@@ -51,24 +51,34 @@ let arr = [7, 7, 7, 7, 2, 4, 6, 8, 2]
     Escribe un programa que imprima el número que tiene más ocurrencias seguidas en arreglo
     y también imprimir la cantidad de veces que aparece en la secuencia.*/
 
-    function ordenar(data){
-      data.sort(function (a, b) {
-        if (a.fechahora < b.fechahora) {
-          return 1;
-        }
-        if (a.fechahora > b.fechahora) {
-          return -1;
-        }
-        return 0;
-      });
-    }
-    function compare(a, b) {
-      if (a < b) {//es menor que b según criterio de ordenamiento
-        return -1;
-      }
-      if (a > b ) {// es mayor que B según criterio de ordenamiento
-        return 1;
-      }
-      // a debe ser igual b
-      return 0;
-    }
+    //let arr = [1,2,2,4,5,6,7,8,8,8];
+    //let i = 0;
+    //let ultimoDato;
+    //function ordenar(data){
+    //    data.sort(function (a, b) {
+    //      if (a > b) {
+    //        return 1;
+    //      }
+    //      if (a < b) {
+    //        return -1;
+    //      }
+    //      return 0;
+    //    });
+    //  }
+    //
+    //ordenar(arr);
+    //
+    //function verOrden(){
+    //  let verUltimo = arr.length -1
+    //  ultimoDato = arr[ verUltimo]
+    //  for (const el in arr){
+    //    if(arr[el] == ultimoDato){
+    //      i = i + 1;
+    //    }
+    //  }
+    //}
+    //
+    //verOrden()
+    //console.log('Longest:', i)
+    //console.log('Number:', ultimoDato)
+    //console.log(arr)
